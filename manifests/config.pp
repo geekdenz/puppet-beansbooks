f9e0cc3ecd807b6a33f0eeeba84e129ff27f2532
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2015-06-22 22:19:39 +1200

    logs writable.

--- manifests/config.pp
+++ manifests/config.pp
@@ -34,10 +34,17 @@ class beansbooks::config {
     require => Postgresql::Server::Db['beans'],
   }
 
-  file { "${beansbooks::dest_path}/application/cache":
+  file { "${beansbooks::dest_path}/application/logs":
     mode => '0666',
     require => File [
       "${beansbooks::dest_path}/application/classes/beans/config.php"
+    ],
+  }
+
+  file { "${beansbooks::dest_path}/application/cache":
+    mode => '0666',
+    require => File [
+      "${beansbooks::dest_path}/application/logs"
     ],
   }
 
