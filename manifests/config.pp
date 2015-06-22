deb1635c30bd5357e06271e0b262c8b28716f82a
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2015-06-22 22:17:05 +1200

    cache writable globally.

--- manifests/config.pp
+++ manifests/config.pp
@@ -34,15 +34,22 @@ class beansbooks::config {
     require => Postgresql::Server::Db['beans'],
   }
 
+  file { "${beansbooks::dest_path}/application/cache":
+    mode => '0666',
+    require => File [
+      "${beansbooks::dest_path}/application/classes/beans/config.php"
+    ],
+  }
+
   check_run::task {'web_install':
-    exec_command => "/usr/bin/php /var/www/beansbooks/index.php --uri=/install/manual\
+    exec_command => "/usr/bin/php index.php --uri=/install/manual\
  --name='${beansbooks::admin_user_full_name}'\
  --password='${beansbooks::admin_user_pass}'\
  --email='${beansbooks::admin_user_email}'\
  --accounts='full'",
     cwd         => $beansbooks::dest_path,
     require => File [
-    "${beansbooks::dest_path}/application/classes/beans/config.php"
+      "${beansbooks::dest_path}/application/cache"
     ],
   }
 
